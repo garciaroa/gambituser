c43b1642bda893b089294b9c644528296e92b7df
garcia roae <cgarciaroa@gmail.com>
2024-02-18 19:37:09 -0500

    Ultimo Commit

--- subo.bat
+++ subo.bat
@@ -3,6 +3,7 @@ git commit -m "Ultimo Commit"
 git push
 set GOOS=linux
 set GOARCH=amd64
-go build main.go -tags -o bootstrap 
+set CGO_ENABLED=0
+go build -tags lambda.norpc -o bootstrap main.go
 del bootstrap.zip
-%USERPROFILE%\Documents\Go\bin\build-lambda-zip.exe -o bootstrap.zip bootstrap
+C:\Users\User\Documents\Go\bin\build-lambda-zip.exe -o bootstrap.zip bootstrap
